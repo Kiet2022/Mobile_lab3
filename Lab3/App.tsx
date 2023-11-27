f8e3e46ca5b72e9031ab330dc41477e7ecae2e5e
Kiet2022 <108647997+Kiet2022@users.noreply.github.com>
2023-11-27 08:25:53 +0700

    pr3 1

--- Lab3/App.tsx
+++ Lab3/App.tsx
@@ -1,13 +1,15 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput, Button, TouchableOpacity} from 'react-native';
+import {View, Text, TextInput, Button, TouchableOpacity, SafeAreaView} from 'react-native';
 import ProductScreen from './Product/Product';
 import AddProduct from './Product/Add';
 import Search from './Product/Search';
+import DetailScreen from './Product/DetailScreen';
+import SearchProduct from './Product/Search';
+
+import styles from './Product/Style';
 const App=()=> {
   return (
-
-    
-    <AddProduct/>
+    <SearchProduct/>
   );
 }
 
--- Lab3/Product/Search.js
+++ Lab3/Product/Search.js
@@ -11,30 +11,97 @@ import {
   } from 'react-native';
   import styles from './Style';
   import {useEffect, useState} from 'react';
-  import {Icon, Appbar} from 'react-native-paper';
 
-function SearchProduct (){
 
-const [data, setData] = useState([]);
-const [value, setValue] = useState('');
-let filePath = 'https://dummyjson.com/products/';
 
-const searchProduct =() =>{
-    if(value !=""){
-        filePath = 'https://dummyjson.com/products/search?q=' + value;
-        fetch(filePath).then((response) =>{
-            if(!response.ok) throw new Error('network is error');
+const ProductScreen = (data) => {  
+    const Item = ({item}) => (
+      <View style={{flex: 1, flexDirection: 'row', paddingHorizontal: 10}}>
+        <View style={{flex: 1}}>
+          <Image
+            source={{uri: item.thumbnail}}
+            style={{width: 120, height: 120}}
+          />
+        </View>
+  
+        <View style={{flex: 2}}>
+          <Text>Description: {item.description}</Text>
+          <Text>Price: {item.price}</Text>
+          <Text>Discount: {item.discountPercentage}</Text>
+          <Text style={{color: 'green'}}>Rating: {item.rating}</Text>
+          <Text>Stock: {item.stock}</Text>
+          <Text>Brand: {item.brand}</Text>
+          <Text>Category: {item.category}</Text>
+  
+          <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Detail</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Add</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Delete</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+    //Alert.alert(data.length.toString());
+    return (
+      <SafeAreaView style={{flex: 1, padding: 10}}>
+        <View>
+          <Text style={styles.Headers}>PRODUCT LIST</Text>
+        </View>
+  
+        <View style={{flex: 3, flexDirection: 'row'}}>
+          <FlatList
+            data={data}
+            renderItem={({item}) => <Item item={item} title={item.title} />}
+          />
+        </View>
+  
+      </SafeAreaView>
+    );
+  };
+  
 
-            return response.json();
-        }).then((d) => {setData(d.products)}).catch((error)=>{console.log(error)})
-    }
+
+const SearchProduct = () => {
+    const [data, setData] = useState([]);
+    const [value, setValue] = useState('');
+    let filePath = 'https://dummyjson.com/products/';
+    
+        const Search =() =>{
+            if(value !=""){
+                filePath = 'https://dummyjson.com/products/search?q=' + value;
+                fetch(filePath).then((response) =>{
+                    if(!response.ok) throw new Error('network is error');
+
+                    return response.json();
+                }).then((d) => {setData(d.products)}).catch((error)=>{console.log(error)})
+            }
+
+            if(data.length == 0){
+                Alert.alert('cannot find');
+            }
+        }
+
+        return(
+            <SafeAreaView style={{flex: 1, padding: 10}}>
+                <Text style={styles.Headers}>Add Product</Text>
+                <View>
+                    <TextInput style={styles.InputBox} placeholder='Enter the title' onChangeText={query => setValue(query)}/>
+                </View>
+                <View>
+                    <TouchableOpacity style={styles.button} >
+                    <Text style={styles.buttonText} onPress={Search}>Submit</Text>
+                    </TouchableOpacity>
+                </View>
+                <ProductScreen data={data}/>
+            </SafeAreaView>
+        );
 }
 
-return(
-    <View>
-
-    </View>
-);
-}
 
 export default SearchProduct;
